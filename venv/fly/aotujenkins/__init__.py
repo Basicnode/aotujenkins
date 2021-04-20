b6a6b8ccfdc4b7ab150c6611c99989eab81e9efb
Zsn <772699450@qq.com>
2021-04-20 09:41:08 +0800

    Initial commit

---
+++ venv/fly/aotujenkins/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/4/20 9:35
+# @Author : XXXX
+# @Email : XXXX@qq.com
+# @File : __init__.py.py
---
+++ venv/fly/aotujenkins/test_aotujenks.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021/4/20 9:37
+# @Author : XXXX
+# @Email : XXXX@qq.com
+# @File : test_aotujenks.py
+import pytest
+import allure
+
+# @pytest.mark.v222
+# @pytest.mark.usefixtures('test001')
+@pytest.fixture(autouse=True)
+# @pytest.fixture()
+def test001():
+    '''newtest workspace'''
+    print("我是公共的",end=',')
+
+def test004():
+    print('---执行004---啦')
+
+class TestCase:
+    def test002(self):
+        print("--hello,i know--")
+
+    def test003(self,x='pears',y='nuts'):
+        s = x + y
+        print(s)
+        assert 'u' in s
+
+
+if __name__ == '__main__':
+#     pytest.main(['-m','v001','-v', '-s'])
+    pytest.main(['-v', '-s'])
